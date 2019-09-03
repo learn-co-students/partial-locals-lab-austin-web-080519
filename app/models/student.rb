6ae0a765db152d91e00199d682cc7222856b5e9d
Anais Ahmed <anaismahmed@uchicago.edu>
2019-09-03 18:19:03 -0500

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,15 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(query)
+    query ||= ''
+    terms = query.split(/\s/)
+    results = Student.all.to_a
+    return results if terms.empty?
+    terms.each do |term|
+      results.delete_if {|student| !(/#{term}/i.match(student.name))}
+    end
+    results
+  end
 end
